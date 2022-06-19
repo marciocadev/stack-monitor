f565cdca2ac0d21423a5137972a1833f97f673d7
Marcio Almeida <marcioadev@gmail.com>
2022-06-19 15:57:43 -0300

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,13 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: '2.28.1',
+  defaultReleaseBranch: 'main',
+  name: 'stack-monitor',
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
