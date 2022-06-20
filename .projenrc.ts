6552e04240ca1aa61635f90c6bef19479e9210a3
Marcio Almeida <marcioadev@gmail.com>
2022-06-19 21:53:14 -0300

    feat: mergify

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,9 +9,23 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   codeCov: true,
   majorVersion: 1,
 
+  githubOptions: {
+    mergify: true,
+    mergifyOptions: {
+      rules: [{
+        name: 'Label core contributions',
+        actions: {
+          label: ['contribution/core'],
+        },
+        conditions: ['author~=^(lazinessdevs)$']
+      }],
+    },
+  },
+
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
